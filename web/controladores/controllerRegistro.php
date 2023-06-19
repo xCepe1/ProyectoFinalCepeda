1ab0bd69b03ab7eb5c1a19951132a944b8b41248
xCepe1 <122973502+xCepe1@users.noreply.github.com>
2023-06-19 11:04:46 +0200

    Update controllerRegistro.php

--- web/controladores/controllerRegistro.php
+++ web/controladores/controllerRegistro.php
@@ -4,8 +4,10 @@ require_once "../modelos/registroModelo.php";
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL); 
+
+
 if (empty($_SESSION['usuario'])) {
-    $registro = new Registro($_POST["dni"], $_POST['password'], $_POST['nombre'], $_POST['email']);
+    $registro = new Registro($_POST["dni"], $_POST['password'], $_POST['nombre'], $_POST['email'],$_POST['fecha'],$_POST['telefono']);
     $respuesta = $registro->registrar();
     if($respuesta == 1){
         echo json_encode($_SESSION['usuario']);
